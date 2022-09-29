884247ed6864f282edda2c55125e194cecfe7b4f
jaeseong2 <jaeseong9810@gmail.com>
2022-09-29 19:27:42 +0900

    feat(game): fix renju rule

--- game/game.py
+++ game/game.py
@@ -20,7 +20,7 @@ class Game(object):
         self.board_size = BOARD_SIZE
         self.black_agent = black_agent
         self.white_agent = white_agent
-        self.directions = [
+        self.direction_sets = [
             [(1, 0), (-1, 0)],
             [(0, 1), (0, -1)],
             [(1, 1), (-1, -1)],
@@ -50,59 +50,54 @@ class Game(object):
 
     def initialize(self):
         self.board = list()
-        self.empty_points = list()
         for i in range(BOARD_SIZE):
             line = list()
             for j in range(BOARD_SIZE):
                 line.append(PointStateEnum.EMPTY)
-                self.empty_points.append((i, j))
             self.board.append(line)
 
         self.forbidden_points = list()
-        self.turn = TurnStateEnum.BLACK
-        self.total_empty_count = BOARD_SIZE * BOARD_SIZE
+        self.current_turn = TurnStateEnum.BLACK
+        self.current_point_state = PointStateEnum.BLACK
+        self.game_state = GameStateEnum.CONTINUE
+        self.empty_point_count = BOARD_SIZE * BOARD_SIZE
 
     def change_turn(self):
-        if self.turn == TurnStateEnum.BLACK:
-            self.turn = TurnStateEnum.WHITE
+        if self.current_turn == TurnStateEnum.BLACK:
+            self.current_turn = TurnStateEnum.WHITE
+            self.current_point_state = PointStateEnum.WHITE
         else:
-            self.turn = TurnStateEnum.BLACK
+            self.current_turn = TurnStateEnum.BLACK
+            self.current_point_state = PointStateEnum.BLACK
 
-    def get_current_point_state(self):
-        state_map = {
-            TurnStateEnum.BLACK: PointStateEnum.BLACK,
-            TurnStateEnum.WHITE: PointStateEnum.WHITE
-        }
-        return state_map[self.turn]
-
-    def is_out_of_board(self, point):
-        row, col = point
+    def is_out_of_board(self, row, col):
         return (
             row < 0 or BOARD_SIZE - 1 < row
             or col < 0 or BOARD_SIZE - 1 < col
         )
 
-    def set_point_state(self, point, state: PointStateEnum):
-        row, col = point
+    def set_point_state(self, row, col, state: PointStateEnum):
         self.board[row][col] = state
 
-    def get_state_count(self, point, state: PointStateEnum, direction):
+    def get_state_count(self, row, col, state: PointStateEnum, directions):
         '''
         Get count of points same with given state
         The count includes start point
         '''
-        row, col = point
-        cnt = 1
-        while True:
-            row, col = row + direction[0], col + direction[1]
-            if self.is_out_of_board(row, col) or self.board[row][col] != state:
-                break
-            else:
-                cnt += 1
-        return cnt
+        count = 1
+        for direction in directions:
+            while True:
+                _row, _col = row + direction[0], col + direction[1]
+                if self.is_out_of_board(_row, _col) or self.board[_row][_col] != state:
+                    break
+                else:
+                    count += 1
+        return count
 
-    def find_empty_point(self, point, state: PointStateEnum, direction):
-        row, col = point
+    def find_empty_point(self, row, col, state: PointStateEnum, direction):
+        '''
+        Find empty point at end of line
+        '''
         while True:
             row, col = row + direction[0], col + direction[1]
             if self.is_out_of_board(row, col) or self.board[row][col] == state.opposite:
@@ -110,136 +105,151 @@ class Game(object):
             if self.board[row][col] == PointStateEnum.EMPTY:
                 return row, col
 
-    def check_open_three(self, point, state, direction):
-        row, col = point
-        for i in range(2):
-            coord = self.find_empty_point(row, col, state, direction * 2 + i)
-            if coord:
-                dx, dy = coord
-                self.set_point_state(dx, dy, state)
-                if 1 == self.open_four(dx, dy, state, direction):
-                    if not self.forbidden_point(dx, dy, state):
-                        self.set_point_state(dx, dy, empty)
-                        return True
-                self.set_point_state(dx, dy, empty)
+    def check_open_three(self, row, col, state, directions):
+        '''
+        Find empty point and set the point as target state
+        Then check the line is open four or not
+        '''
+        for direction in directions:
+            empty_point = self.find_empty_point(row, col, state, direction)
+            if empty_point:
+                empty_row, empty_col = empty_point
+                self.set_point_state(empty_row, empty_col, state)
+                if (
+                    self.check_open_four(empty_row, empty_col, state, directions)
+                    and not self.check_forbidden_point(empty_row, empty_col, state)
+                ):
+                    self.set_point_state(empty_row, empty_col, PointStateEnum.EMPTY)
+                    return True
+                self.set_point_state(empty_row, empty_col, PointStateEnum.EMPTY)
         return False
 
-    def check_open_four(self, point, state, direction):
-        row, col =point
-        if self.is_five(x, y, state):
+    def check_open_four(self, row, col, state, directions):
+        if self.check_five(row, col, state):
             return False
-        cnt = 0
-        for i in range(2):
-            coord = self.find_empty_point(x, y, state, direction * 2 + i)
-            if coord:
-                if self.check_five(coord[0], coord[1], state, direction):
-                    cnt += 1
-        if cnt == 2:
-            if 4 == self.get_state_count(x, y, state, direction):
-                cnt = 1
-        else: cnt = 0
-        return cnt
+        count = 0
+        for direction in directions:
+            empty_point = self.find_empty_point(row, col, state, direction)
+            if empty_point:
+                empty_row, empty_col = empty_point
+                if self.check_five(empty_row, empty_col, state, direction):
+                    count += 1
+        if count == 2:
+            if self.get_state_count(row, col, state, directions) == 4:
+                count = 1
+        else:
+            count = 0
+        return count
 
-    def check_four(self, point, state, direction):
-        row, col = point
-        for i in range(2):
-            coord = self.find_empty_point(x, y, state, direction * 2 + i)
-            if coord:
-                if self.check_five(coord[0], coord[1], state, direction):
+    def check_four(self, row, col, state, directions):
+        '''
+        Check open and closed four
+        '''
+        for direction in directions:
+            empty_point = self.find_empty_point(row, col, state, direction)
+            if empty_point:
+                if self.check_five(empty_point[0], empty_point[1], state, directions):
                     return True
         return False
 
-
-    def check_five(self, point, state, direction):
-        if self.get_state_count(self, point, state, direction) == 5:
+    def check_five(self, row, col, state, directions):
+        if self.get_state_count(self, row, col, state, directions) == 5:
             return True
         return False
 
-    def check_six(self, point, state, direction):
-        if self.get_state_count(self, point, state, direction) > 5:
+    def check_six(self, row, col, state, directions):
+        if self.get_state_count(self, row, col, state, directions) > 5:
             return True
         return False
 
-    def check_double_three(self, point, state):
-        cnt = 0
-        row, col = point
+    def check_double_three(self, row, col, state):
+        '''
+        Check given point is double three point in all directions
+        '''
+        count = 0
         self.set_point_state(row, col, state)
-        for i in range(4):
-            if self.open_three(row, col, state, i):
-                cnt += 1
-        self.set_point_state(row, col, empty)
-        if cnt >= 2:
-            # print("double three")
+        for directions in self.direction_sets:
+            if self.check_open_three(row, col, state, directions):
+                count += 1
+        self.set_point_state(row, col, PointStateEnum.EMPTY)
+        if count >= 2:
             return True
         return False
 
-    def check_double_four(self, point, state):
-
-        cnt = 0
-        self.set_point_state(x, y, state)
-        for i in range(4):
-            if self.open_four(x, y, state, i) == 2:
-                cnt += 2
-            elif self.four(x, y, state, i):
-                cnt += 1
-        self.set_point_state(x, y, empty)
-        if cnt >= 2:
-            # print("double four")
+    def check_double_four(self, row, col, state):
+        '''
+        Check given point is double three point
+        
+        '''
+        count = 0
+        self.set_point_state(row, col, state)
+        for directions in self.direction_sets:
+            if self.check_open_four(row, col, state, directions) == 2:
+                count += 2
+            elif self.check_four(row, col, state, directions):
+                count += 1
+        self.set_point_state(row, col, PointStateEnum.EMPTY)
+        if count >= 2:
             return True
         return False
 
+    def check_forbidden(self, row, col):
+        if self.check_five(row, col, PointStateEnum.BLACK):
+            return False
+        elif self.check_six(row, col, PointStateEnum.BLACK):
+            return True
+        elif (
+            self.double_three(row, col, PointStateEnum.BLACK)
+            or self.double_four(row, col, PointStateEnum.BLACK)
+        ):
+            return True
+        return False
 
-    def set_forbidden_points(self, point):
-        row, col = point
+    def set_forbidden_points(self, row, col):
         start_row, end_row = max(0, row - 5), min(BOARD_SIZE, row + 5)
         start_col, end_col = max(0, col - 5), min(BOARD_SIZE, col + 5)
 
         for row in range(start_row, end_row):
             for col in range(start_col, end_col):
                 if self.board[row][col] == PointStateEnum.EMPTY:
-                    if self.check_forbidden((row, col)):
+                    if self.check_forbidden(row, col):
                         self.board[row][col] = PointStateEnum.FORBIDDEN
                         self.forbidden_points.append((row, col))
 
                 if self.board[row][col] == PointStateEnum.FORBIDDEN:
-                    if not self.check_forbidden((row, col)):
+                    if not self.check_forbidden(row, col):
                         self.board[row][col] = PointStateEnum.EMPTY
                         self.forbidden_points.remove((row, col))
 
+    def check_finished(self, row, col):
+        if self.current_turn == TurnStateEnum.BLACK:
+            if self.check_five(row, col, PointStateEnum.BLACK):
+                self.game_state = GameStateEnum.BLACK
+                return True
+            left = self.empty_point_count - len(self.forbidden_points)
+        else:
+            if self.check_five(row, col, PointStateEnum.WHITE):
+                self.game_state = GameStateEnum.WHITE
+                return True
+            left = self.empty_point_count
+        if left == 0:
+            self.game_state = GameStateEnum.DRAW
+            return True
+        return False
 
     def start(self):
         while True:
             try:
                 for move_function in self.move_functions:
-                    if self.turn == TurnStateEnum.BLACK:
-                        possible = set(self.empty_points).difference(
-                            set(self.forbidden_points)
-                        )
+                    row, col = move_function(self.board)
+                    self.set_point_state(row, col, self.current_point_state)
+                    self.empty_point_count -= 1
+                    self.check_finished(row, col)
+                    if self.current_turn == TurnStateEnum.BLACK:
+                        self.set_forbidden_points(row, col)
                     else:
-                        possible = set(self.empty_points)
-                    row, col = move_function(self.board, possible)
-                    point = (row, col)
-                    self.set_point_state(
-                        point,
-                        self.get_current_point_state()
-                    )
-                    self.empty_points.remove(point)
-                    self.total_empty_count -= 1
-
-                    if self.turn == TurnStateEnum.BLACK:
-                        self.check_finished(
-                            self.total_empty_count - len(self.forbidden_points),
-                            point
-                        )
-                        self.set_forbidden_points(point)
-                    else:
-                        self.check_finished(
-                            self.total_empty_count,
-                            point
-                        )
                         if self.board[row][col] == PointStateEnum.FORBIDDEN:
-                            self.forbidden_points.remove(point)
-                        self.set_selectable_points()
+                            self.forbidden_points.remove((row, col))
                     self.change_turn()
 
             except KeyboardInterrupt:
