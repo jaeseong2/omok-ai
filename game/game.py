050ebbc44a5d505cbf7d3f499d97b5a4aa4644ef
jaeseong2 <jaeseong9810@gmail.com>
2022-10-01 12:41:52 +0900

    feat(game): remove game end exception

--- game/game.py
+++ game/game.py
@@ -2,7 +2,6 @@ from enums import GameMode, PointStateEnum, TurnStateEnum, GameStateEnum
 from .exc import (
     OutOfIndexError,
     CanNotSelectError,
-    GameEndError,
     TestEndError,
 )
 from config import BOARD_SIZE
